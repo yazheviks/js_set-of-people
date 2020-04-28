feb5cd20822adb0c173eb920d02323cc4eb13f8e
mgrinko <glukym@gmail.com>
2020-04-28 16:55:57 +0300

    hotfix

--- src/setOfPeople.test.js
+++ src/setOfPeople.test.js
@@ -10,12 +10,12 @@ describe('setOfPeople', () => {
 
   test('Single person', () => {
     expect(setOfPeople([23]))
-      .toEqual(new Set(23));
+      .toEqual(new Set([23]));
   });
 
   test('Single person reentering', () => {
     expect(setOfPeople([23, 23, 23]))
-      .toEqual(new Set(23));
+      .toEqual(new Set([23]));
   });
 
   test('Single person leaving', () => {
@@ -25,19 +25,19 @@ describe('setOfPeople', () => {
 
   test('Sample test', () => {
     expect(setOfPeople([1, 3, 2, 1, 2, 1]))
-      .toEqual([3, 1]);
+      .toEqual(new Set([3, 1]));
   });
 
   test('5 people, no one leaving', () => {
     expect(setOfPeople([3, 2, 5, 4, 1]))
-      .toEqual(new Set(3, 2, 5, 4, 1));
+      .toEqual(new Set([3, 2, 5, 4, 1]));
   });
 
   test('10 people, 20 events', () => {
     const people = [3, 1, 8, 8, 2, 1, 9, 0, 3, 6, 9, 1, 5, 0, 2, 0, 1, 8, 5, 7];
 
     expect(setOfPeople(people))
-      .toEqual(new Set(6, 0, 8, 7));
+      .toEqual(new Set([6, 0, 8, 7]));
   });
 
   test('20 people, 100 events', () => {
@@ -50,6 +50,6 @@ describe('setOfPeople', () => {
     ];
 
     expect(setOfPeople(people))
-      .toEqual(new Set(18, 0, 16, 4, 17, 12, 10, 3, 14, 11, 2, 7, 6, 1));
+      .toEqual(new Set([18, 0, 16, 4, 17, 12, 10, 3, 14, 11, 2, 7, 6, 1]));
   });
 });
